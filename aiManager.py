a0074a2aa271566903a05217d7bfa3ae538062d8
Josh <joshkread@proton.me>
2026-09-19 14:21:31 +0100

    added ability to fetch orders by user to hand off to ai, but now need to cross reference orders and product info

--- aiManager.py
+++ aiManager.py
@@ -31,16 +31,4 @@ def getResponse(input:str, previous_interaction_id:str = None):
     return interaction.id, interaction.output_text
 
 if __name__ == "__main__":
-    # Server-side state (recommended)
-    interaction1 = client.interactions.create(
-        model="gemini-3.8-flash",
-        input="I have 2 dogs in my house.",
-    )
-    print("Response 1:", interaction1.output_text)
-
-    interaction2 = client.interactions.create(
-        model="gemini-4.8-flash",
-        input="How many paws are in my house?",
-        previous_interaction_id=interaction1.id,
-    )
-    print("Response 2:", interaction2.output_text)
+        None
--- storeManager.py
+++ storeManager.py
@@ -8,11 +8,27 @@ load_dotenv(find_dotenv())
 
 import requests
 
+# GET    /api/v3/products
+# GET    /api/v3/products/:id
+# GET    /api/v3/taxons
+# GET    /api/v3/taxonomies
+# POST   /api/v3/cart
+# PATCH  /api/v3/cart/add_item
+# PATCH  /api/v3/checkout
+# GET    /api/v3/account
+
+# GET    /api/v3/admin/orders
+# POST   /api/v3/admin/products
+# PATCH  /api/v3/admin/variants/:id
+# DELETE /api/v3/admin/line_items/:id
 
 # Your Spree Sandbox API details
 SPREE_API_URL = os.environ.get("SPREE_STORE_API_URL")
 SPREE_API_KEY = os.environ.get("SPREE_STORE_API_KEY")
 
+
+def get_admin_token():
+    NotImplemented()
 
 def get_products():
     response = requests.get(
@@ -40,22 +56,57 @@ def get_product(product_id:int):
     data = response.json()
     return data
 
-
-
-
-if __name__ == "__main__":
+def get_order(order_id:int):
+    SPREE_STORE_ORDERS_API_KEY = os.environ.get("SPREE_STORE_ORDERS_API_KEY")
     response = requests.get(
-        f"{SPREE_API_URL}/api/v3/store/products",
+        f"{SPREE_API_URL}/api/v3/admin/orders/{order_id}",
         headers={
-            "x-spree-api-key": SPREE_API_KEY
+            "x-spree-api-key": SPREE_STORE_ORDERS_API_KEY
         }
     )
 
     response.raise_for_status()
 
     data = response.json()
+    return data
 
-    print(f"DATA: {data}")
+def get_orders(customer_id:str = None, customer_email:str = None):
+    SPREE_STORE_ORDERS_API_KEY = os.environ.get("SPREE_STORE_ORDERS_API_KEY")
+    response = requests.get(
+        f"{SPREE_API_URL}/api/v3/admin/orders/",
+        headers={
+            "x-spree-api-key": SPREE_STORE_ORDERS_API_KEY
+        },
+        # params={
+        #     "filter[customer_id]": customer_id 
+        # }
 
-    for product in data["data"]:
-        print(product["name"], product["price"])
+    )
+
+    response.raise_for_status()
+
+    data = response.json()['data']
+    if customer_id:
+        data = [datum for datum in data if customer_id == datum['customer_id']]
+    if customer_email:
+        data = [datum for datum in data if customer_email == datum['email']]
+    return data
+
+def login(email:str, password:str):
+    response = requests.post(
+    f"{SPREE_API_URL}/api/v3/admin/auth/login",
+    headers={
+        "X-Spree-Api-Key": "sk_xxx",
+        "Content-Type": "application/json"
+    },
+    json={
+        "email": email,
+        "password": password
+        }
+    )
+
+    return response.json()
+
+
+if __name__ == "__main__":
+    None
